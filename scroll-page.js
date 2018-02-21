f65bfde57c8bbc849f65226a1cfcc99d68d0fed0
Julio Lira <jul10l1r4@gmail.com>
2018-02-21 02:30:04 -0300

    766 Bytes de puro Javascriptizinho, coisa que teria muito maaaisss

--- scroll-page.js
+++ scroll-page.js
@@ -6,4 +6,4 @@ const go = (elemento) => {// Arrow function levando um parâmetro que será um s
 	  left: 0,// Obrigatório, nao queremos movimentos horizontais
 	  behavior: 'smooth' // Unico efeito, dá a instrução ao browser que ele movaaaa uhuuull caraaai
   });// terminouuu fiii, só isso, c n vai precisar exporrtar mais 5000mil instruçoes de dados da porra do jquery
-} // Só seeeeu, fiiii
+} // Só seeeeu,_fiiii
