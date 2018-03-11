bf30bb22daeac25462b3bab2267a0b44485a7e0b
Julio Lira <jul10l1r4@gmail.com>
2018-03-11 06:10:54 -0300

    Update Top.js

--- Top.js
+++ Top.js
@@ -19,7 +19,7 @@ const vai = ( a, b ) => {// passando valores com poucos como letras.. menos dado
 const go = (elemento) => {// Arrow function levando um parâmetro que será um seletor como o de css
   let vivo = document.querySelector(elemento)// Pegará esse seletor e retornará um elemento vivo
   window.scroll({ // Função nativa dos browsers
-	  top: vivo.getBoundingClientRect().top, // Captura a quantidade precisa de px do elemento em cada tela diferente fiii
+	  top: vivo.offsetTop, // Captura a quantidade precisa de px do elemento em cada tela diferente fiii
 	  left: 0,// Obrigatório, nao queremos movimentos horizontais
 	  behavior: 'smooth' // Unico efeito, dá a instrução ao browser que ele movaaaa uhuuull caraaai
   });// terminouuu fiii, só isso, c n vai precisar exporrtar mais 5000mil instruçoes de dados da porra do jquery
